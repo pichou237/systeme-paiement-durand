290cb6da56650f222cbc03b097f98da8a86b67d1
K-CM12 <149551550+K-CM12@users.noreply.github.com>
2025-07-02 20:31:41 +0200

    origin

--- offstore/offstore/settings.py
+++ offstore/offstore/settings.py
@@ -195,3 +195,4 @@ STRIPE_WEBHOOK_SECRET = config("STRIPE_WEBHOOK_SECRET")
 SITE_URL = "http://localhost:8000"
 
 CSRF_TRUSTED_ORIGINS = ["https://systeme-paiement-durand.onrender.com"]
+CSRF_COOKIE_SECURE = True
